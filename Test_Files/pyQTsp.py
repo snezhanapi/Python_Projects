217360f373791950275d3ce3c3259faed15df986
Snejana <snezhanapirdopska@gmail.com>
2022-04-20 22:21:24 +0300

    Initial

---
+++ Test_Files/pyQTsp.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication,QWidget,QLabel,QPushButton
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle('PyQt6 App Works')
+#comment
+
+window.setGeometry(100, 100, 500, 500)
+
+window.show()
+sys.exit(app.exec())
