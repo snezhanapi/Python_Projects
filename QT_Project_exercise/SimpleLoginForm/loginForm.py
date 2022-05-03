23a2154be684191590977389db1be57744c4fb09
Snejana <snezhanapirdopska@gmail.com>
2022-05-03 20:06:37 +0300

    Initial commit

---
+++ QT_Project_exercise/SimpleLoginForm/loginForm.py
@@ -0,0 +1,24 @@
+from PyQt6 import QtWidgets as qtw
+from PyQt6 import QtCore as qtc
+from PyQt6 import QtGui as qtg
+from SimpleLoginForm.Ui_form import Ui_Form
+
+class LoginForm(Ui_Form):
+
+	def __init__(self , *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		self.setupUi(self)
+		self.btnSubmit.clicked.connect( self.onBtnSubmitClick )
+
+	@qtc.pyqtSlot(bool)
+	def onBtnSubmitClick(self):
+		user_name = self.leUserName.text()
+		user_pasword = self.lePass.text()
+		print(user_name, user_pasword)
+
+		# HW: add your code, which will check if user exists in DB
+
+
+if __name__ == '__main__':
+	pass
--- QT_Project_exercise/app.py
+++ QT_Project_exercise/app.py
@@ -1,54 +1,18 @@
 import sys
 from PyQt6 import QtWidgets as qtw
-from PyQt6 import QtCore as qtc
-from PyQt6 import QtGui as qtg
+from SimpleLoginForm.loginForm import LoginForm
 
-from LoginForm import LoginForm
+class MainWindow(qtw.QWidget, LoginForm):
+	def __init__(self):
+		super().__init__()
 
-class MainWindow(qtw.QWidget):
+		# add as any properties here...
+		self.setWindowTitle('The title of main window')
 
-	def __init__(self , *args, **kwargs):
-		super().__init__(*args, **kwargs)
+if __name__ == "__main__":
+  app = qtw.QApplication(sys.argv)
 
-		self.setWindowTitle('Demo App')
+  main = MainWindow()
+  main.show()
 
-		# create widgets
-		btnRegistration = qtw.QPushButton('Registration')
-		btnLogIn = qtw.QPushButton('LogIn')
-
-		# create main layout
-		main_layout = qtw.QHBoxLayout(self)
-		main_layout.addWidget(btnRegistration)
-		main_layout.addWidget(btnLogIn)
-		# main_layout.setStretch(0, 200)
-
-
-		# on event fired  = > eventHandlerFunction()
-		# signal emmited  => slot()
-		# btnLogIn click  => onBtnLogInClick()
-
-		### one object an have many signals
-		# btnLogIn.clicked.connect( lambda : print('Click') )
-		# btnLogIn.pressed.connect( lambda : print('Pressed') )
-		# btnLogIn.released.connect( lambda : print('Released') )
-
-		### One signal can be connected to one or many slots
-		# btnLogIn.clicked.connect( lambda: print('Hello') )
-
-		btnLogIn.clicked.connect( self.onBtnLogInClick )
-
-		self.show();
-
-	def onBtnLogInClick(self):
-		print('btnLogIn was clicked')
-		self.loginForm = LoginForm()
-		self.loginForm.show()
-
-
-
-if __name__ == '__main__':
-	app = qtw.QApplication(sys.argv);
-
-	window = MainWindow()
-
-	sys.exit(app.exec())
+  sys.exit(app.exec())
